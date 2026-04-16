84f5f8737669840a197744b90c7a06bea161eb0e
ktlichkid <liqingsheng@pku.edu.cn>
2026-04-17 02:11:06 +0900

    [SWE_01] Add BPE runtime encode/decode for issue #8

--- src/small_scale_llm/tokenizer/__init__.py
+++ src/small_scale_llm/tokenizer/__init__.py
@@ -6,9 +6,12 @@ from .bpe_training import (
     train_bpe_from_tinystories,
     write_bpe_artifact,
 )
+from .runtime import BPETokenizer, load_bpe_tokenizer
 
 __all__ = [
     "BPEArtifact",
+    "BPETokenizer",
+    "load_bpe_tokenizer",
     "train_bpe_from_texts",
     "train_bpe_from_tinystories",
     "write_bpe_artifact",
